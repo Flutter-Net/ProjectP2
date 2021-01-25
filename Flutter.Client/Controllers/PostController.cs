969815fec7569e4ce56d13f401d7de234f44c2bb
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-01-25 14:55:25 -0500

    Feed and Post Controllers and Views moved into Project Repo

---
+++ Flutter.Client/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Flutter.Client.Models;
+
+namespace Flutter.Client.Controllers
+{
+    [Route("[controller]")]
+
+    public class PostController : Controller
+    {
+        
+
+        [HttpGet]
+        public IActionResult Post()
+        {
+            return View("Post");
+        }
+
+      
+    }
+}
