8b0cab25978c4f2928b08a5e164b1c1588ddd181
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-01-25 17:27:42 -0500

    refactored Add

--- Flutter.Storing/FlutterRepository.cs
+++ Flutter.Storing/FlutterRepository.cs
@@ -13,16 +13,16 @@ namespace Flutter.Storing
         {
             _ctx = context;
         }
-        public void AddUser(string name, string password){
-            var user = new AUser(name,password);
+        public void AddUser(AUser user){
+            
             _ctx.Users.Add(user);
         }
-        public void AddPost(long inputUserId, string inputContent, long CommentOf, IEnumerable<long> Tags){
-            var post = new Post(inputUserId,inputContent,CommentOf,Tags);
+        public void AddPost(Post post){
+            
             _ctx.Users.Add(post);
         }
-        public void AddTag(string name){
-            var tag = new Tag(name);
+        public void AddTag(Tag tag){
+            
             _ctx.Tags.Add(tag);
         }
         // public IEnumerable<Store> GetStores()
