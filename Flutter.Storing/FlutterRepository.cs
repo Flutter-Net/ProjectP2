6e156c1550759b5b3816434089a3a3d87c560805
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-01-25 19:41:40 -0500

    commit premerge with law

--- Flutter.Storing/FlutterRepository.cs
+++ Flutter.Storing/FlutterRepository.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Flutter.Domain.Abstracts;
+using Flutter.Domain.Models;
 
 namespace Flutter.Storing
 {
@@ -13,43 +15,14 @@ namespace Flutter.Storing
         {
             _ctx = context;
         }
-        public void AddUser(AUser user){
-            
+        // public void Add<T>(T item) where T :AEntity
+        // {
+
+        //     _ctx.Set<T>.Add(item);
+        // }
+        public void AddUser(AUser user)
+        {
             _ctx.Users.Add(user);
         }
-        public void AddPost(Post post){
-            
-            _ctx.Users.Add(post);
-        }
-        public void AddTag(Tag tag){
-            
-            _ctx.Tags.Add(tag);
-        }
-        // public IEnumerable<Store> GetStores()
-        // {
-        //     return _ctx.Stores;
-        // }
-        // public IEnumerable<User> GetUsers()
-        // {
-        //     return _ctx.Users;
-        // }
-        // public User GetUser(long id)
-        // {
-        //     return _ctx.Users.Include(user => user.Orders).ThenInclude(order => order.Pizzas).ThenInclude(pizza => pizza.Crust).
-        //     Include(user => user.Orders).ThenInclude(order => order.Pizzas).ThenInclude(pizza => pizza.Size).
-        //     Include(user => user.Orders).ThenInclude(Order => Order.Pizzas).ThenInclude(pizza => pizza.Toppings).
-        //     FirstOrDefault(user => user.EntityId == id);
-        // }
-        // public Post GetPost(string id)
-        // {
-        //     return _ctx.Stores.Include(store => store.Orders).ThenInclude(order => order.Pizzas).ThenInclude(pizza => pizza.Crust).
-        //     Include(store => store.Orders).ThenInclude(order => order.Pizzas).ThenInclude(pizza => pizza.Size).
-        //     Include(store => store.Orders).ThenInclude(Order => Order.Pizzas).ThenInclude(pizza => pizza.Toppings).
-        //     FirstOrDefault(Store => Store.EntityId == long.Parse(id));
-        // }
-        // public void Update()
-        // {
-        //     _ctx.SaveChanges();
-        // }
     }
 }
