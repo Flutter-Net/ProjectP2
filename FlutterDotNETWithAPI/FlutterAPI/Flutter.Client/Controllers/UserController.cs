a2e4f74ab01c656cb35c2b4ab822a4cdf219eda0
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-01-30 10:01:45 -0500

    lots of gets

--- FlutterDotNETWithAPI/FlutterAPI/Flutter.Client/Controllers/UserController.cs
+++ FlutterDotNETWithAPI/FlutterAPI/Flutter.Client/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Flutter.Domain.Models;
 using Flutter.Storing;
+using Microsoft.EntityFrameworkCore;
 
 namespace Flutter.Client.Controllers
 {
@@ -23,14 +24,69 @@ namespace Flutter.Client.Controllers
 
 
         [HttpGet("/users")]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> GetUsers()
         {
             var users = _ctx.Users.ToList();
 
             return await Task.FromResult(Ok(users));
 
         }
-        
+        [HttpGet("/user/{name}")]
+        public IActionResult GetUser(string name)
+        {
+            var user = _ctx.Users.Include(u => u.Posts).FirstOrDefault(user => user.Name == name);
+
+            return Ok(user);
+        }
+        [HttpGet("/user/{name}/posts")]
+        public IActionResult GetUserPosts(string name)
+        {
+          var user = _ctx.Users.FirstOrDefault(user => user.Name == name);
+          var posts = _ctx.Posts.Where( post => post.UserId == user.EntityId);
+            return Ok(posts);
+        }
+
+        [HttpGet("/posts")]
+        public IActionResult GetPosts()
+        {
+            var posts = _ctx.Posts.ToList();
+            return Ok(posts);
+        }
+
+        [HttpGet("/post/{id}")]
+        public IActionResult GetPost(long id)
+        {
+            var post = _ctx.Posts.FirstOrDefault(post => post.EntityId == id);
+
+            return Ok(post);
+        }
+
+        [HttpGet("/Tags")]
+        public IActionResult GetTags()
+        {
+            var tags = _ctx.Tags.ToList();
+
+            return Ok(tags);
+        }
+
+        [HttpGet("/Tag/{name}")]
+        public IActionResult GetTag(string name)
+        {
+            var tag = _ctx.Tags.FirstOrDefault(tag => tag.TagName == name);
+
+            return Ok(tag);
+        }
+        [HttpGet("/Post/Tags/{id}")]
+        public IActionResult GetPostTags(long id)
+        {
+            var post = _ctx.Posts.Include(p => p.TagIds).FirstOrDefault(p => p.EntityId == id);
+            List<Tag> output = new List<Tag>();
+            foreach (Tag tag in post.TagIds)
+            {
+                output.Add(tag);
+            }
+            return Ok(output);
+        }
     }
 }
 
