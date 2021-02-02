d952f5bd799fe741b6f9fdc4c2ce16971b181c7b
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-02-02 14:05:22 -0500

    commiting working front-end branch just in case

--- FlutterDotNETWithAPI/FlutterWeb/FlutterWeb.Client/Models/PostViewModel.cs
+++ FlutterDotNETWithAPI/FlutterWeb/FlutterWeb.Client/Models/PostViewModel.cs
@@ -5,5 +5,7 @@ namespace FlutterWeb.Client.Models
         public long UserId { get; set; }
         public string Content { get; set; }
         public long CommentOf { get; set; }
+
+        public long PostId {get;set;}
     }
 }
