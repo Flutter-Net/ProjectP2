b2718b594f7040ee1cd1acc89de2ffa53cccb6e8
Darren Yakes <dayakes@gmail.com>
2021-02-03 15:00:30 -0500

    fixed merge conflict

--- FlutterDotNETWithAPI/FlutterWeb/FlutterWeb.Client/Controllers/AccountController.cs
+++ FlutterDotNETWithAPI/FlutterWeb/FlutterWeb.Client/Controllers/AccountController.cs
@@ -3,11 +3,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Okta.AspNetCore;
 
-<<<<<<< HEAD
- [Route("[controller]")]
-=======
 [Route("[controller]")]
->>>>>>> ebd7818752fb3e751d60376c5f5580457495022d
 public class AccountController : Controller
 {
     public IActionResult SignIn()
